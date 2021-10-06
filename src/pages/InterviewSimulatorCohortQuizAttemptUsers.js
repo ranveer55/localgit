5739a4a6f3c6235352aa5363d2748890b8f5ac2c
Shubham Singh Chahar <thefallenmerc@gmail.com>
2021-10-06 16:15:52 +0530

    added quiz based reports

---
+++ src/pages/InterviewSimulatorCohortQuizAttemptUsers.js
@@ -0,0 +1,149 @@
+import moment from "moment";
+import React, { Component } from "react";
+import { Link } from 'react-router-dom';
+import { Line } from "react-chartjs-2";
+import ReactDatePicker from "react-datepicker";
+
+export const interviewSimulatorCohortQuizAttemptUsersRoute = "/interview-simulator/:cohortId/quiz-attempt-users";
+
+// function to create route
+export const createInterviewSimulatorCohortQuizAttemptUsersRoute = cohortId => `/interview-simulator/${cohortId}/quiz-attempt-users`;
+
+class InterviewSimulatorCohortQuizAttemptUsersPage extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.cohortId = props.match.params.cohortId;
+
+        this.companyCode = global.companyCode;
+
+        this.state = {
+            dateLoaded: false,
+            cohort: null,
+            dateLoaded: false,
+            startDate: new Date(moment().subtract(1, "week")),
+            endDate: new Date(moment()),
+            users: [],
+            dataSets: null,
+        };
+
+        this.state.selectedCompany = global.companyCode;
+        this.state.selectedCompanyName = global.companyName;
+    }
+
+    componentDidMount() {
+        // getCompanyRegistrationReport 
+        this.loadData(this.state.startDate, this.state.endDate);
+
+
+    }
+    downloadCSV(data) {
+
+        //define the heading for each row of the data  
+        var csv = ['Email', 'First Name', 'Last Name', 'Questions Answered', 'Attempts', 'Asked for Review', 'Reviews Completed', '\n'].join(",");
+
+        //merge the data with CSV  
+        data.forEach(function (row) {
+            csv += row.join(',');
+            csv += "\n";
+        });
+        var hiddenElement = document.createElement('a');
+        hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);
+        hiddenElement.target = '_blank';
+
+        //provide the name for the CSV file to be downloaded  
+        hiddenElement.download = `${this.state.cohort.name}.csv`;
+        hiddenElement.click();
+    }
+
+    loadData(startDate, endDate) {
+        this.setState({
+            dateLoaded: false
+        });
+        global.api.getCompanyCohortUsersWhoAttemptedQuiz(
+            this.cohortId
+        )
+            .then(
+                data => {
+                    this.setState({
+                        dataLoaded: true,
+                        cohort: data.program,
+                        users: data.users,
+                    });
+                    // this.setState({ batchData: data });
+                })
+            .catch(err => {
+                this.setState({
+                    dateLoaded: true
+                });
+            });
+    }
+
+
+
+    render() {
+
+        const cohort = this.state.cohort;
+
+
+        return (
+            <main className="offset" id="content">
+                <div className="row">
+                    <div className="">
+                        <h4 className="title4 mb40">
+                            For {this.state?.cohort?.name}
+                        </h4>
+                        <br />
+                    </div>
+
+                </div>
+
+                <section className="section_box">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <h1 className="title1 mb25">Company Cohorts</h1>
+                            <h4 className="title4 mb40">
+                                {
+                                    cohort ? (
+                                        <>
+                                            <div><b>Cohort:</b> {this.state.cohort.name}</div>
+                                            <table className="table" style={{ width: '100%' }}>
+                                                <thead>
+                                                    <tr>
+                                                        <th style={{ width: '250px' }}>Email</th>
+                                                        <th>First Name</th>
+                                                        <th>Last Name</th>
+                                                        <th>Attempts</th>
+                                                        <th>View Details</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    {
+                                                        this.state.users.map((user) => (
+                                                            <tr>
+                                                                <td style={{ wordBreak: "break-all" }}>{user.userId}</td>
+                                                                <td>{user.FirstName}</td>
+                                                                <td>{user.LastName}</td>
+                                                                <td>{user.attemptCount}</td>
+                                                                <td><a href={"/interview-simulator/" + this.cohortId + "/quiz-attempt-users/" + user.userId + "/log"}>Show Attempts Log</a></td>
+                                                            </tr>
+                                                        ))
+                                                    }
+                                                </tbody>
+                                            </table>
+                                        </>
+                                    ) : <>No Data</>
+                                }
+                            </h4>
+                        </div>
+                    </div>
+                    <div>
+                    </div>
+                </section>
+            </main>
+        );
+    }
+}
+
+export default InterviewSimulatorCohortQuizAttemptUsersPage;
