b56980f81e09a20bcfe440ecd20ff46ab041ea58
amitjsdev <amit.oficial92@gmail.com>
2022-01-18 23:40:35 +0530

    setup video uplaod and record video

---
+++ src/config/mixpanel.config.js
@@ -0,0 +1,24 @@
+import mixpanel from 'mixpanel-browser';
+
+mixpanel.init('59bae692077ad0378690050996ef40c9');
+
+let envCheck = process.env.NODE_ENV === 'production';
+
+let actions = {
+    identify: (id) => {
+        if (envCheck) mixpanel.identify(id);
+    },
+    alias: (id) => {
+        if (envCheck) mixpanel.alias(id);
+    },
+    track: (eventName, props) => {
+        if (envCheck) mixpanel.track('InterviewPrep.' + eventName, props);
+    },
+    people: {
+        set: (props) => {
+            if (envCheck) mixpanel.people.set(props);
+        },
+    },
+};
+
+export let mixPanel = actions;
