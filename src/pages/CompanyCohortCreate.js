41122879495632e4771ad4db4fadd9f7514a9bed
lakhvinder <lkm1developer@gmail.com>
2022-01-22 18:11:11 +0530

    timezonne fix

--- src/pages/CompanyCohortCreate.js
+++ src/pages/CompanyCohortCreate.js
@@ -1,4 +1,4 @@
-import moment from "moment";
+import moment from 'moment-timezone'
 import React, { Component } from "react";
 import Datetime from 'react-datetime';
 import "react-datetime/css/react-datetime.css";
